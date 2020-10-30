cd99f2d22ea9807ccea297bdf5a70c82c006024d
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2020-10-30 15:30:33 +1100

    Add some more attraction data

--- borderless_tourist.py
+++ borderless_tourist.py
@@ -53,3 +53,20 @@ def add_attraction(destination, attraction):
 # print(attractions)
 add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
 print(attractions)
+
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", [
+               "historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", [
+               "garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", [
+               "skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", [
+               "monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+print(attractions)
